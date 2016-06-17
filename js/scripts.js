07cf1d6b501d848b4997678c4339bf2c00b9197a
Brett New <brettnew@Bretts-MacBook-Air.local>
2016-06-17 12:36:57 -0500

    Fix bug with branching in js to allow multiple toppings to be selected.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ function Pizza(name, size, toppings) {
 
 Pizza.prototype.price = function () {
   var cost = 6;
-  if (this.size === "Medium"){
+  if (this.size === "Small"){
+    cost += 0;
+  } else if (this.size === "Medium"){
     cost += 2;
   } else if (this.size === "Large") {
     cost += 2;
@@ -18,23 +20,32 @@ Pizza.prototype.price = function () {
   }
   if (this.toppings[0] === "extra-cheese") {
     cost += .75;
-  } else if (this.toppings[1] === "pepperoni") {
-    cost += 1
-  } else if (this.toppings[2] === "sausage") {
-    cost += 1
-  } else if (this.toppings[3] === "ham") {
-    cost += 1
-  } else if (this.toppings[4] === "bacon") {
-    cost += 1
-  } else if (this.toppings[5] === "onions") {
-    cost += .5
-  } else if (this.toppings[6] === "greenpeppers") {
-    cost += .5
-  } else if (this.toppings[7] === "tomatoes") {
-    cost += .5
-  } else if (this.toppings[8] === "pineapple") {
-    cost += 1
   }
+  if (this.toppings[1] === "pepperoni") {
+    cost += 1;
+  }
+  if (this.toppings[2] === "sausage") {
+    cost += 1;
+  }
+  if (this.toppings[3] === "ham") {
+    cost += 1;
+  }
+  if (this.toppings[4] === "bacon") {
+    cost += 1;
+  }
+  if (this.toppings[5] === "onions") {
+    cost += .5;
+  }
+  if (this.toppings[6] === "greenpeppers") {
+    cost += .5;
+  }
+  if (this.toppings[7] === "tomatoes") {
+    cost += .5;
+  }
+  if (this.toppings[8] === "pineapple") {
+    cost += 1;
+  }
+  return cost;
 };
 
 
@@ -44,12 +55,14 @@ $(document).ready(function(){
   $("#pizza-form").submit(function(event){
     event.preventDefault();
 
-    var newPizza = new Pizza (toppings);
     var name = $("input.user-name").val();
     var size = $("select.pizza-size").val();
     var toppings = [$("#extra-cheese:checked").val(),$("#pepperoni:checked").val(),$("#sausage:checked").val(),$("#ham:checked").val(),$("#bacon:checked").val(),$("#onions:checked").val(),$("#greenpeppers:checked").val(),$("#tomatoes:checked").val(),$("#pineapple:checked").val()]
 console.log(toppings)
-    newPizza.price();
+console.log(size)
+console.log(name)
+    var newPizza = new Pizza (name, size, toppings);
+    console.log(newPizza.price());
 
   });
 });
